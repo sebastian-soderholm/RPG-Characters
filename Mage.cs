9f26f6a07b8def5e7dacd8d16121fdbc2a24bd15
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-08-15 11:38:08 +0300

    Added template implementations of character methods

--- Mage.cs
+++ Mage.cs
@@ -7,6 +7,39 @@ namespace RPG_Characters
 {
     public class Mage : Character
     {
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int Level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override StringBuilder StatsStringBuilder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override Dictionary<Slot, Item> Equipments { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+        public override void Attack(Character targetCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Equip(Weapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Equip(Armor armor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EquipPermitted(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float GetDPS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LevelUp()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

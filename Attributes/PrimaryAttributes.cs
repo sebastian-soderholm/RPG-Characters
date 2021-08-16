01cdf0c3e2fbb47238672f2a30955da0a89fa12f
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-08-16 22:07:42 +0300

    Overloaded +, ==, != operators and Equals() method  in PrimaryAttribute & SecondaryAttribute classes

--- Attributes/PrimaryAttributes.cs
+++ Attributes/PrimaryAttributes.cs
@@ -13,6 +13,37 @@ namespace RPG_Characters
         public int Intelligence { get; set; }
         public int Vitality { get; set; }
 
+        public static PrimaryAttributes operator + (PrimaryAttributes lhs, PrimaryAttributes rhs)
+            => new PrimaryAttributes(
+                lhs.Strength + rhs.Strength,
+                lhs.Dexterity + rhs.Dexterity,
+                lhs.Intelligence + rhs.Intelligence,
+                lhs.Vitality + rhs.Vitality);
+        public static PrimaryAttributes operator +(PrimaryAttributes lhs, int rhs)
+            => new PrimaryAttributes(
+                lhs.Strength + rhs,
+                lhs.Dexterity + rhs,
+                lhs.Intelligence + rhs,
+                lhs.Vitality + rhs);
+        public static bool operator == (PrimaryAttributes lhs, PrimaryAttributes rhs)
+            => lhs.Strength == rhs.Strength &&
+                lhs.Dexterity == rhs.Dexterity &&
+                lhs.Intelligence == rhs.Intelligence &&
+                lhs.Vitality == rhs.Vitality;
+        public static bool operator != (PrimaryAttributes lhs, PrimaryAttributes rhs)
+            => lhs.Strength != rhs.Strength &&
+                lhs.Dexterity != rhs.Dexterity &&
+                lhs.Intelligence != rhs.Intelligence &&
+                lhs.Vitality != rhs.Vitality;
+        public override bool Equals(Object a)
+        {
+            return a is PrimaryAttributes b &&
+                Strength == b.Strength &&
+                Dexterity == b.Dexterity &&
+                Intelligence == b.Intelligence &&
+                Vitality == b.Vitality;
+        }
+
         public PrimaryAttributes(int strength, int dexterity, int intelligence, int vitality)
         {
             Strength = strength;
--- Attributes/SecondaryAttributes.cs
+++ Attributes/SecondaryAttributes.cs
@@ -11,6 +11,32 @@ namespace RPG_Characters
         public int Health { get; set; }
         public int ArmorRating { get; set; }
         public int ElementalResistance { get; set; }
+        public static SecondaryAttributes operator + (SecondaryAttributes lhs, SecondaryAttributes rhs)
+            => new SecondaryAttributes(
+                lhs.Health + rhs.Health,
+                lhs.ArmorRating + rhs.ArmorRating,
+                lhs.ElementalResistance + rhs.ElementalResistance);
+        public static SecondaryAttributes operator + (SecondaryAttributes lhs, int b)
+            => new SecondaryAttributes(
+                lhs.Health + b,
+                lhs.ArmorRating + b,
+                lhs.ElementalResistance + b);
+        public static bool operator == (SecondaryAttributes lhs, SecondaryAttributes rhs)
+            =>  lhs.Health == rhs.Health &&
+                lhs.ArmorRating == rhs.ArmorRating &&
+                lhs.ElementalResistance == rhs.ElementalResistance;
+        public static bool operator != (SecondaryAttributes lhs, SecondaryAttributes rhs)
+            => lhs.Health != rhs.Health &&
+                lhs.ArmorRating != rhs.ArmorRating &&
+                lhs.ElementalResistance != rhs.ElementalResistance;
+        public override bool Equals(Object a)
+        {
+            return a is SecondaryAttributes b &&
+                Health == b.Health &&
+                ArmorRating == b.ArmorRating &&
+                ElementalResistance == b.ElementalResistance;
+        }
+
 
         public SecondaryAttributes(int health, int armorRating, int elementalResistance)
         {
